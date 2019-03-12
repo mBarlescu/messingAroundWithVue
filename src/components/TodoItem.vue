70cbc298f2a83c94c1912fcbef0a623b2cc2b3da
Mark Barlescu <barlescu92@gmail.com>
2019-03-11 21:20:21 -0700

    had props passed down to components and added a checkbox event

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,13 +1,22 @@
 <template>
-  <div>
-    <p>Todo Item</p>
+  <div class='todo-item' v-bind:class="{'is-complete': todo.completed}">
+    <p>
+    <input type="checkbox" v-on:change='markComplete'>
+    {{todo.title}}
+    </p>
   </div>
 </template>
 
 <script>
 
 export default{
-  name: 'TodoItem'
+  name: 'TodoItem',
+  props: ['todo'],
+  methods: {
+    markComplete(){
+      this.todo.completed = !this.todo.completed
+    }
+  }
 }
 
 
